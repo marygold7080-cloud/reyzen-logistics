ee3a3d4b02644add0d946713b1e125724ec284c4
marygold7080-cloud <marygold7080@gmail.com>
2026-08-28 04:46:06 +0500

    Bypass CDN cache with unique chunk names and add no-cache headers

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/reyzen-app-[hash].js`,
+        chunkFileNames: `assets/reyzen-chunk-[hash].js`,
+        assetFileNames: `assets/reyzen-style-[hash].[ext]`
+      }
+    }
+  },
   server: {
     port: 3000,
     open: false
